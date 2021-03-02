72608e7bbfabad5873f4ab37662809dbf33b68e6
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-03-01 18:13:01 -0700

    added prompt questions

---
+++ Develop/index.js
@@ -0,0 +1,78 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
+
+
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// TODO: Create an array of questions for user input
+const questions = ["What is the title of your project?", "Please describe your project:", "Please describe any installation instructions:", "Please describe the usage of this project:", "Please choose a license:", "Are there any contributors to this project?", "How can someone test this project?", "Please enter your GitHub username:"];
+
+const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[0],
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[1],
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: questions[2],
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: questions[3],
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: questions[4],
+            choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: questions[5],
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: questions[6],
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: questions[7],
+        }
+    ]);
+
+promptUser()
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
